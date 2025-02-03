ec5cff307f92af50e1748624e71ecfa768476862
mominovDev <mardonmominov6@gmail.com>
2025-02-03 15:08:57 +0500

    many files and footer fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import About from './pages/About/About'
 import Header from './components/Header/Herader';
 import Footer from './components/Footer/Footer';
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' index element={<Home/>}/>
+        <Route path='/about' index element={<About/>}/>
         <Route path='*' index element={<Navigate to='/' replace/>}/>
       </Routes>
       <Footer/>
