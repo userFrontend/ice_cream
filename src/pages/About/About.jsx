7ce047079319ebc3060a3eaa3d27ba0a8d9f3e85
mominovDev <mardonmominov6@gmail.com>
2025-02-04 20:33:47 +0500

    many changes added.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,39 +1,185 @@
 import React from "react";
+import "./About.scss";
+import Slider from "react-slick";
+
 
 const Home = () => {
+    const settings1 = {
+        infinite: true, // Cheksiz aylanish
+        slidesToShow: 8, // Bir vaqtning o'zida 1 slayd
+        slidesToScroll: 1,
+        autoplay: true, // Avtomatik aylanish
+        speed: 1000,
+        autoplaySpeed: 1000,
+        cssEase: "linear"
+      };
+      const settings2 = {
+        rtl:true,
+        infinite: true, // Cheksiz aylanish
+        slidesToShow: 8, // Bir vaqtning o'zida 1 slayd
+        slidesToScroll: 1,
+        autoplay: true, // Avtomatik aylanish
+        speed: 1000,
+        autoplaySpeed: 1000,
+        cssEase: "linear"
+      };    
   return (
-    <section className="section section__bg_2 section__not_off home">
-      <div className="section-row">
-        <div className="section-col">
-          <div className="section-logo">
-            <picture>
-              <img
-                src="https://nmedov.uz/wp-content/uploads/2022/11/logo-full-3.svg"
-                width="566"
-                alt="Logo"
-              />
-            </picture>
+    <>
+      <section className="second">
+        <div className="container second-container">
+          <div className="second-container__left">
+            <img data-aos="fade-up" src="/images/logo.png" alt="" />
+            <h2 data-aos="fade-up">
+              logoni pastiga text bering <br />
+            </h2>
+            <p data-aos="fade-up">batafsil</p>
           </div>
-          <p>
-            Sifat barqarorligi, o‘z vaqtida yetkazib berish, ta’minotchilar va
-            hamkorlar bilan uzoq muddatli va ishonchli munosabatlar
-          </p>
-        </div>
-        <div className="section-col">
-          <div className="section-image">
-            <picture>
-              <img
-                className="lazyload"
-                src="https://nmedov.uz/wp-content/uploads/2023/03/choco.png"
-                width="628"
-                height="780"
-                alt="Chocolate"
-              />
-            </picture>
+          <div className="second-container__right" data-aos="fade-up">
+            <img src="/images/choco.png" alt="" />
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="third">
+        <div className="container third-container">
+          <div className="third-container__up">
+            <h1 data-aos="fade-right">{">15"} </h1>
+            <h3 data-aos="fade-left">
+              O‘zbekistonda 15 dan ortiq yetakchi brendlar
+            </h3>
+          </div>
+          <div className="third-container__down">
+            <Slider {...settings1}>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+            </Slider>
+            <Slider {...settings2}>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="/images/logo.png" alt="Slide 1" />
+              </div>
+            </Slider>
+    
+          </div>
+        </div>
+      </section>
+      <section className="messiya">
+        <div className="container messiya-container">
+          <div className="messiya-container__left">
+            <h1 data-aos="fade-down">{">20"} </h1>
+            <p data-aos="fade-up">
+              bozorda 20 yildan ortiq tajriba <br />
+            </p>
+            <a href="/" data-aos="fade-up">
+              batafsil
+            </a>
+          </div>
+          <div className="messiya-container__right" data-aos="fade-up">
+            <h3>
+              Yuqori texnologik uskunalar va 20 yillik ish tajribasi bizga
+              tansiq ta’m, Yevropa sifati va arzon narxlarni o‘zida mujassam
+              etuvchi yuqori sifatli oziq-ovqat mahsulotlarini ishlab chiqarish
+              imkoniyatini beradi.
+            </h3>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
