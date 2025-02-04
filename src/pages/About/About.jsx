053f37b8e8defbd8092ecb5f8745894b7de22aa5
mominovDev <mardonmominov6@gmail.com>
2025-02-04 10:37:47 +0500

    comit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+
 const Home = () => {
   return (
-    <div className="shior">
-      <div className="right set-card__info">
-        <img src="/images/logo.png" alt="logo-img" />
-        <p>
-          Sifat barqarorligi, o‘z vaqtida yetkazib berish, ta’minotchilar va
-          hamkorlar bilan uzoq muddatli va ishonchli munosabatlar
-        </p>
+    <section className="section section__bg_2 section__not_off home">
+      <div className="section-row">
+        <div className="section-col">
+          <div className="section-logo">
+            <picture>
+              <img
+                src="https://nmedov.uz/wp-content/uploads/2022/11/logo-full-3.svg"
+                width="566"
+                alt="Logo"
+              />
+            </picture>
+          </div>
+          <p>
+            Sifat barqarorligi, o‘z vaqtida yetkazib berish, ta’minotchilar va
+            hamkorlar bilan uzoq muddatli va ishonchli munosabatlar
+          </p>
+        </div>
+        <div className="section-col">
+          <div className="section-image">
+            <picture>
+              <img
+                className="lazyload"
+                src="https://nmedov.uz/wp-content/uploads/2023/03/choco.png"
+                width="628"
+                height="780"
+                alt="Chocolate"
+              />
+            </picture>
+          </div>
+        </div>
       </div>
-      <div className="set-card__img left">
-        <img src="/images/choco.png" alt="right-img" />
-      </div>
-    </div>
+    </section>
   );
 };
 
