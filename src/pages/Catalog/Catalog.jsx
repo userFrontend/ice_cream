bcc6ef18ceaf89fce6cc96844ae35e8c2d4dbb14
AzizbekGitHub99 <azizbekxotamaliyev@gmail.com>
2025-02-07 21:59:07 +0500

    catalog

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,9 +1,266 @@
-import "./Catalog.scss"
+import "./Catalog.scss";
 
 const Catalog = () => {
   return (
-    <div>Catalog</div>
-  )
-}
+    <>
+      <sectiion className="catalog">
+        <div className="container catalog-container">
+          <h1 className="catalog-container__title">{`Mahsulotlar ro'yhati`}</h1>
+          <div className="catalog-container__wrapper">
+            <h1 className="catalog-container__wrapper__title">Muzqaymoqlar</h1>
+            <div className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Kreasy Max
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Kreasy Max Berry</p>
+                </a>
+              </div>
+            </div>
+            <div className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Rojok
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="catalog-container__wrapper">
+            <h1 className="catalog-container__wrapper__title">Siroklar</h1>
+            <div className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Sirok Batoncha
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+              </div>
+            </div>
+            <div className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Sirok Mockup
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </sectiion>
+    </>
+  );
+};
 
-export default Catalog
+export default Catalog;
