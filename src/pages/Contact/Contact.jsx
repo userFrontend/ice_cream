234b35d85c248ad5362063d562bbfb32189bb38c
mominovDev <mardonmominov6@gmail.com>
2025-02-11 15:05:34 +0500

    catalog page changed by mardon

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,99 +1,280 @@
-import React, { useState } from "react";
-import './Contact.scss'
-const Home = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    subject: "",
-    message: "",
-  });
+import "./Catalog.scss";
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data Submitted:", formData);
-    // Add your API request logic here
-    alert("Form submitted successfully!");
-  };
+const Catalog = () => {
   return (
-    <div className="contact">
-      <div className="container">
-        <div className="info-contact" data-aos="zoom-in">
-          <h2 data-aos="fade-right">Контакты</h2>
-          <b>Адрес:</b>
-          <p>OOO "SABE", г. Сырдарья, Сырдарьинская область, туманный центр Рахимов, улица Достлик, дом 20.</p>
-          <b>Телефон:</b>
-          <p>+99899 055 82 83</p>
+    <>
+      <sectiion className="catalog">
+        <div className="container catalog-container">
+          <h1 data-aos="fade-left" className="catalog-container__title">
+            {`Список продуктов`}{" "}
+            <a
+              href="/images/ahmedov_katalog.pdf"
+              download="ahmedov_katalog.pdf"
+            >
+              Yuklash <i className="bx bx-down-arrow-alt"></i>
+            </a>
+          </h1>
+          <div data-aos="fade-up" className="catalog-container__wrapper">
+            <h1 className="catalog-container__wrapper__title">Мороженое</h1>
+            <div data-aos="fade-up" className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Кризи Макс
+              </h2>
+              <div
+                data-aos="fade-up"
+                className="catalog-container__wrapper__box__row"
+              >
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/kreasymaxberry.png" alt="iceCream" />
+                  <p>Кризи Макс Berry</p>
+                </a>
+              </div>
+            </div>
+            <div data-aos="fade-up" className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Rojok
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok1.png" alt="iceCream" />
+                  <p>Rojok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/rojok2.png" alt="iceCream" />
+                  <p>Rojok Cherry</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div data-aos="fade-up" className="catalog-container__wrapper">
+            <h1 className="catalog-container__wrapper__title">Siroklar</h1>
+            <div data-aos="fade-up" className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Sirok Batoncha
+              </h2>
+              <div className="catalog-container__wrapper__box__row">
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokbaton.png" alt="iceCream" />
+                  <p>Sirok Caramel</p>
+                </a>
+              </div>
+            </div>
+            <div data-aos="fade-up" className="catalog-container__wrapper__box">
+              <h2 className="catalog-container__wrapper__box__subtitle">
+                Sirok Mockup
+              </h2>
+              <div
+                data-aos="fade-up"
+                className="catalog-container__wrapper__box__row"
+              >
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+                <a
+                  href="/catalog"
+                  className="catalog-container__wrapper__box__row__item"
+                >
+                  <img src="images/sirokmockup.png" alt="iceCream" />
+                  <p>Sirok Plombir</p>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-     <div className="contact-form-container">
-      <div className="container">
-      <h2 data-aos="fade-right">Связаться с нами</h2>
-      <form onSubmit={handleSubmit} className="form-contact">
-        <div className="contact-form">
-        <div className="form-group" data-aos="fade-up">
-          <label>Ваше имя</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Укажите имя"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group" data-aos="fade-up">
-          <label>Номер телефона</label>
-          <input
-            type="tel"
-            name="phone"
-            placeholder="+998 90 999-99-99"
-            value={formData.phone}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group" data-aos="fade-up">
-          <label>Электронная почта</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="example@mail.com"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group" data-aos="fade-up">
-          <label>Тема сообщения</label>
-          <input
-            type="text"
-            name="subject"
-            placeholder="Укажите тему"
-            value={formData.subject}
-            onChange={handleChange}
-          />
-        </div>
-        </div>
-        <div className="form-groups" data-aos="fade-up">
-          <label>Сообщение</label>
-          <textarea
-            name="message"
-            placeholder="Ваше сообщение"
-            value={formData.message}
-            onChange={handleChange}
-          />
-        </div>
-        <button data-aos="zoom-in" type="submit" className="submit-button">
-          Отправить
-        </button>
-      </form>
-      </div>
-    </div>
-    </div>
+      </sectiion>
+    </>
   );
 };
 
-export default Home;
+export default Catalog;
